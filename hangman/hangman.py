0230111f1a3601d6db46b5e49e5dca78cfc178ac
Pedro Calleja <pecalleja@gmail.com>
2021-06-22 08:22:34 -0500

    single choice

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,3 +1,8 @@
 # Write your code here
 print("H A N G M A N")
-print("The game will be available soon.")
+print("Guess the word:")
+text = input()
+if text == "python":
+    print("You survived!")
+else:
+    print("You lost!")
