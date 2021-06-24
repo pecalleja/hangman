c29cc60b0edce9641a073cd3cf85ffc08b45ec4d
Pedro Calleja <pecalleja@gmail.com>
2021-06-23 21:36:25 -0500

    do not count repeated letters

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,5 +1,6 @@
 # Write your code here
 import random
+import string
 print("H A N G M A N")
 options = ['python', 'java', 'kotlin', 'javascript']
 winner = random.choice(options)
@@ -8,13 +9,17 @@ res = {letter: [] for letter in winner}
 for idx, letter in enumerate(winner):
     res[letter].append(idx)
 count = 0
+tries = set()
 while count < 8:
     print()
     print("".join(hidden))
     letter = input("Input a letter:")
-    if letter in hidden:
-        count += 1
-        print("No improvements")
+    if len(letter) == 1 and letter not in string.ascii_letters.lower():
+        print("Please enter a lowercase English letter")
+    elif len(letter) != 1:
+        print("You should input a single letter")
+    elif letter in tries:
+        print("You've already guessed this letter")
     elif letter in winner:
         indexes = res[letter]
         for idx in indexes:
@@ -24,7 +29,9 @@ while count < 8:
             print("You guessed the word!")
             print("You survived!")
             break
+        tries.add(letter)
     else:
+        tries.add(letter)
         count += 1
         print("That letter doesn't appear in the word")
 else:
