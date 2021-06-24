6d98bb389d6b385e7129233563318656b7e4cc64
Pedro Calleja <pecalleja@gmail.com>
2021-06-23 20:16:01 -0500

    add choice

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,8 +1,11 @@
 # Write your code here
+import random
 print("H A N G M A N")
 print("Guess the word:")
 text = input()
-if text == "python":
+options = ['python', 'java', 'kotlin', 'javascript']
+winner = random.choice(options)
+if text in winner:
     print("You survived!")
 else:
     print("You lost!")
