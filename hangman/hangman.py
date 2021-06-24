23587a6f5debbe7be4b6b38b72ebc91174a61dcb
Pedro Calleja <pecalleja@gmail.com>
2021-06-23 21:43:34 -0500

    add tge neby

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -2,37 +2,50 @@
 import random
 import string
 print("H A N G M A N")
-options = ['python', 'java', 'kotlin', 'javascript']
-winner = random.choice(options)
-hidden = ["-"]*len(winner)
-res = {letter: [] for letter in winner}
-for idx, letter in enumerate(winner):
-    res[letter].append(idx)
-count = 0
-tries = set()
-while count < 8:
-    print()
-    print("".join(hidden))
-    letter = input("Input a letter:")
-    if len(letter) == 1 and letter not in string.ascii_letters.lower():
-        print("Please enter a lowercase English letter")
-    elif len(letter) != 1:
-        print("You should input a single letter")
-    elif letter in tries:
-        print("You've already guessed this letter")
-    elif letter in winner:
-        indexes = res[letter]
-        for idx in indexes:
-            hidden[idx] = letter
-        if "-" not in hidden:
-            print("".join(hidden))
-            print("You guessed the word!")
-            print("You survived!")
-            break
-        tries.add(letter)
+
+
+def display_menu():
+    menu = input('Type "play" to play the game, "exit" to quit:')
+    if menu == "play":
+        start_game()
+
+
+def start_game():
+    options = ['python', 'java', 'kotlin', 'javascript']
+    winner = random.choice(options)
+    hidden = ["-"]*len(winner)
+    res = {letter: [] for letter in winner}
+    for idx, letter in enumerate(winner):
+        res[letter].append(idx)
+    count = 0
+    tries = set()
+    while count < 8:
+        print()
+        print("".join(hidden))
+        letter = input("Input a letter:")
+        if len(letter) == 1 and letter not in string.ascii_letters.lower():
+            print("Please enter a lowercase English letter")
+        elif len(letter) != 1:
+            print("You should input a single letter")
+        elif letter in tries:
+            print("You've already guessed this letter")
+        elif letter in winner:
+            indexes = res[letter]
+            for idx in indexes:
+                hidden[idx] = letter
+            if "-" not in hidden:
+                print("".join(hidden))
+                print("You guessed the word!")
+                print("You survived!")
+                break
+            tries.add(letter)
+        else:
+            tries.add(letter)
+            count += 1
+            print("That letter doesn't appear in the word")
     else:
-        tries.add(letter)
-        count += 1
-        print("That letter doesn't appear in the word")
-else:
-    print("You lost!")
+        print("You lost!")
+    display_menu()
+
+
+display_menu()
